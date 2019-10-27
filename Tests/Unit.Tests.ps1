4b2d764360a4575a85bf22ed7d175d9ad0272e84
kevinmarquette <kevmar@gmail.com>
2019-10-26 21:24:20 -0700

    Add unit test

---
+++ Tests/Unit.Tests.ps1
@@ -0,0 +1,12 @@
+Describe 'Unit Tests' -Tag Build {
+    BeforeAll {
+        Push-Location TestDrive:
+    }
+    AfterAll {
+        Pop-Location
+    }
+
+    It 'New-KmtProject should create a new module'  {
+        New-KmtProject -Path ($pwd) -Name TestModule
+    }
+}
